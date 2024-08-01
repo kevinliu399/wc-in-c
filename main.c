44de7d57481924c40544d9f0b73a452aaff8aac5
kevinliu399 <kevinliu399@gmail.com>
2024-07-31 23:23:22 -0400

    finished countlines

--- main.c
+++ main.c
@@ -26,18 +26,23 @@ int main(int argc, char *argv[])
     char *has_wflag = NULL;
     char *has_mflag = NULL;
     char *all_flags[4] = {has_cflag, has_lflag, has_wflag, has_mflag};
-    char flag_type[] = {'c', 'l', 'm', 'w'};
+    char flag_type[] = {'c', 'l', 'w', 'm'};
     long (*function_array[])(FILE *fileptr) = {
         compute_total_bytes,
+        compute_total_lines,
         compute_total_words,
-        compute_total_chars,
-        compute_total_lines
-    };
+        compute_total_chars,};
     bool has_flag = false;
     char *flag = NULL;
     char result_buffer[256] = "";
-    FILE* file;
+    FILE *file;
     long results[4] = {0, 0, 0, 0};
+
+    if (argc < 3)
+    {
+        fprintf(stderr, "Usage: %s <flags> <filename>\n", argv[0]);
+        return 1;
+    }
 
     if (argc > 2)
     {
@@ -49,19 +54,22 @@ int main(int argc, char *argv[])
         flag = argv[1];
         // TODO: Print error for invalid flags
 
-
         file = fopen(argv[2], "r");
-        if (file == NULL) {
+        if (file == NULL)
+        {
             perror("Error opening file");
             return 1;
         }
 
         // Compute all the flags
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++)
+        {
             all_flags[i] = strchr(flag, flag_type[i]);
-            if (all_flags[i] != NULL) {
+            if (all_flags[i] != NULL)
+            {
+                rewind(file); // reset file pointer
                 results[i] = function_array[i](file);
-                char temp[64];  // temp buffer for each result
+                char temp[64]; // temp buffer for each result
                 snprintf(temp, sizeof(temp), "%c: %ld ", flag_type[i], results[i]);
                 strcat(result_buffer, temp); // add the temp result to the buffer
             };
@@ -88,9 +96,31 @@ long compute_total_bytes(FILE *fileptr)
 }
 
 // TODO
-long compute_total_words(FILE *fileptr)
+long compute_total_lines(FILE *fileptr)
 {
-    return 0;
+    long line_count = 0;
+    char line[1024];
+
+    while (fgets(line, sizeof(line), fileptr) != NULL)
+    {
+        line_count++;
+
+        // Check if the line was too long
+        if (line[strlen(line) - 1] != '\n' && !feof(fileptr))
+        {
+            int ch;
+            while ((ch = fgetc(fileptr)) != EOF && ch != '\n')
+                ;
+        }
+    }
+
+    if (ferror(fileptr))
+    {
+        fprintf(stderr, "An error occurred while reading the file.\n");
+        return -1;
+    }
+
+    return line_count;
 }
 
 long compute_total_chars(FILE *fileptr)
@@ -98,7 +128,7 @@ long compute_total_chars(FILE *fileptr)
     return 0;
 }
 
-long compute_total_lines(FILE *fileptr)
+long compute_total_words(FILE *fileptr)
 {
     return 0;
 }
